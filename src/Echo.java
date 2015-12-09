9e2c2b2593289f4555e25a9c9245af4eb1b220f9
deanshmuel <dshmuel110@gmail.com>
2015-12-09 23:07:41 +0200

    Adding Echo- The original chat program

---
+++ src/Echo.java
@@ -0,0 +1,104 @@
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.text.DefaultCaret;
+
+public class Echo implements ActionListener, Runnable {
+
+	private static final String HOST = "10.0.0.1";
+	private static final int PORT = 12345;
+	private final JTextField tf;
+	private final JTextArea ta;
+	private final JButton send;
+	private volatile PrintWriter out;
+	private Scanner in;
+	private Thread thread;
+	private Kind kind;
+
+	public static enum Kind {
+
+		Client(100, "Trying"), Server(500, "Awaiting");
+		private int offset;
+		private String activity;
+
+		private Kind(int offset, String activity) {
+			this.offset = offset;
+			this.activity = activity;
+		}
+	}
+
+	public Echo(Kind kind, JTextField tf, JTextArea ta, JButton send) {
+		this.kind = kind;
+		this.tf = tf;
+		this.ta = ta;
+		this.send = send;
+		send.addActionListener(this);
+		ta.setLineWrap(true);
+		ta.setWrapStyleWord(true);
+		DefaultCaret caret = (DefaultCaret) ta.getCaret();
+		caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
+		// display(kind.activity + HOST + " on port " + PORT);
+		thread = new Thread(this, kind.toString());
+	}
+
+	public void start() {
+		thread.start();
+	}
+
+	// @Override
+	public void actionPerformed(ActionEvent ae) {
+		// if boolen->String s = image_text()
+		String s = tf.getText();
+		if (out != null) {
+			out.println(s);
+		}
+		display(s);
+		tf.setText("");
+	}
+
+	// @Override
+	public void run() {
+		try {
+			Socket socket;
+			if (kind == Kind.Client) {
+				socket = new Socket(HOST, PORT);
+			} else {
+				ServerSocket ss = new ServerSocket(PORT);
+				socket = ss.accept();
+			}
+			in = new Scanner(socket.getInputStream());
+			out = new PrintWriter(socket.getOutputStream(), true);
+			display("Connected");
+			while (true) {
+				display(in.nextLine());
+			}
+		} catch (Exception e) {
+			display(e.getMessage());
+			e.printStackTrace(System.err);
+		}
+	}
+
+	private void display(final String s) {
+		EventQueue.invokeLater(new Runnable() {
+			// @Override
+			public void run() {
+				ta.append(s + "\n");
+			}
+		});
+	}
+
+	/*
+	 * public static void main(String[] args) { EventQueue.invokeLater(new
+	 * Runnable() { //@Override public void run() { new
+	 * Echo(Kind.Server).start(); } }); }
+	 */
+
+}
